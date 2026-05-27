35cdfc4d7c39be71e7f1cea3c301d935dfd07deb
Dibyansh Mohanty <mohantydibyansh77@gmail.com>
2026-05-27 21:29:58 +0530

    Implement basic UI for YouTube video summarization

---
+++ frontend/app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import requests
+
+st.title("🎥 AI YouTube Video Summarizer")
+
+youtube_url = st.text_input("Enter YouTube URL")
+
+summarize_button = st.button("Generate Summary")
+
+if summarize_button:
+    response = requests.post(
+    "http://127.0.0.1:8000/summarize",
+    json={
+        "youtube_url": youtube_url
+    })
+    data = response.json()
+
+    if "summary" in data:
+        st.subheader("Summary")
+        st.write(data["summary"])
+
+    else:
+        st.error(data["detail"])
